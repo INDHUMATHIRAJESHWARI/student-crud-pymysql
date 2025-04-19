accadc434ff1dd1498747868aacd8033d174b340
INDHUMATHIRAJESHWARI <mathiindhu1346174@gmail.com>
2025-04-19 13:20:28 +0530

    feat(ui): added console-based student management menu i.e Student registration (ID, name, age), Record viewing/editing/deletion, Application exit handling, -Basic input error catching, -Depends on database.py for operations

---
+++ main.py
@@ -0,0 +1,33 @@
+#Console Intreface
+import database
+try:
+    db=database.crud()
+    while True:
+        print("\n\n DashBoard")
+        print("1. Insert\n2. View\n3. Edit\n4. Delete\n0. Close")
+        ch=int(input("Enter choice : "))
+        if ch==1:
+            print("\n Student Registration Form")
+            i=int(input("Enter id : "))
+            n=input("Enter name : ")
+            a=int(input("Enter age : "))
+            db.add(i,n,a)
+        elif ch==2:
+            print("\n Registered Student Data")
+            db.view()
+        elif ch==3:
+            print("\n Student Updation Form")
+            i=int(input("Enter id : "))
+            a=int(input("Enter age : "))
+            db.edit(i,a)
+        elif ch==4:
+            print("\nStudent Removal Form")
+            i=int(input("Enter id : "))
+            db.delete(i)
+        elif ch==0:
+            db.cancel()
+            break
+        else:
+            print("\n Invalid Choice")
+except Exception as a:
+    print("\n Error ~ ",a)
